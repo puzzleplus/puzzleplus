930cfb7c51fe47091c1b992eb9a73f4f048ff44e
Dan Vanderkam <danvdk@gmail.com>
2010-03-16 23:39:27 -0700

    update thumbnail hosts as well

--- change_host.sh
+++ change_host.sh
@@ -4,3 +4,5 @@ echo Setting include base to "$base"...
 perl -i -pe 's,(type="text/css" href=").*(/crossword\.css"/>),$1'$base'$2,g' wave/lmnowave.xml
 
 perl -i -pe 's,(<script src=").*(/[^/]*\.js"></script>),$1'$base'$2,g' wave/lmnowave.xml
+
+perl -i -pe 's,((?:thumbnail|screenshot)=").*(/[^/]*\.png"),$1'$base'$2,g' wave/lmnowave.xml
