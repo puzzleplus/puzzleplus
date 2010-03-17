ea68320fbfe0f829c72625fa781dcb2216514273
Dan Vanderkam <danvdk@gmail.com>
2010-03-16 23:08:16 -0700

    add script to change the host

---
+++ change_host.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+base=$1;
+echo Setting include base to "$base"...
+perl -i -pe 's,(type="text/css" href=").*(/crossword\.css"/>),$1'$base'$2,g' wave/lmnowave.xml
+
+perl -i -pe 's,(<script src=").*(/[^/]*\.js"></script>),$1'$base'$2,g' wave/lmnowave.xml
