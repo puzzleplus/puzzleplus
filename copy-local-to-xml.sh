5c9d4bb3d0db853806e7603f57a9d10eb7675b03
Dan Vanderkam <dan@dygraphs.com>
2012-05-17 11:02:17 -0400

    use new icons

--- copy-local-to-xml.sh
+++ copy-local-to-xml.sh
@@ -6,7 +6,8 @@ perl -ne 'print if /<body>/../<\/body>/' localtest/test.html > /tmp/body
 perl -ne 'print if 1../<body>/' wave/lmnowave.xml > /tmp/prologue
 perl -ne 'print if /<\/body>/..1' wave/lmnowave.xml > /tmp/postlogue
 
-perl -i -pe 's,../wave/([^.]*)\.png,https://raw.github.com/danvk/lmnowave/master/wave/\1.png,g' /tmp/body
+perl -i -pe 's,../wave/([^.]*)\.png,https://github.com/danvk/lmnowave/raw/master/wave/\1.png,g' /tmp/body
+perl -i -pe 's,../icons/([^.]*)\.png,https://github.com/danvk/lmnowave/raw/master/icons/\1.png,g' /tmp/body
 
 cat /tmp/prologue <(sed 1d /tmp/body) <(sed 1d /tmp/postlogue) > wave/lmnowave.xml
 
