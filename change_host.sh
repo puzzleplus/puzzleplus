2bd119cf630e47cfccc5f67ed0cf205b6847deb6
Dan Vanderkam <danvdk@gmail.com>
2014-12-09 00:30:47 -0500

    clear out some unused files

--- change_host.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-base=$1;
-echo Setting include base to "$base"...
-perl -i -pe 's,(type="text/css" href=").*(/crossword\.css"/>),$1'$base'$2,g' wave/lmnowave.xml
-
-perl -i -pe 's,(<script src=").*(/[^/]*\.js"></script>),$1'$base'$2,g' wave/lmnowave.xml
-
-perl -i -pe 's,((?:thumbnail|screenshot)=").*(/[^/]*\.png"),$1'$base'$2,g' wave/lmnowave.xml
-perl -i -pe 's,(src=").*?(/[^/]*\.png"),$1'$base'$2,g' wave/lmnowave.xml
